96009f2b11b51127e33532e538693205bb421279
RajAmin99 <rajamin2004@gmail.com>
2026-04-22 11:43:28 +0100

    Lennard Jones plot

--- LennardJonesPotential/LennardJonesPotential.py
+++ LennardJonesPotential/LennardJonesPotential.py
@@ -3,10 +3,22 @@ Lennard-Jones potential plotted in Matplotlib
 """
 import numpy as np
 import matplotlib.pyplot as plt
+from typing import final
 
 
 def main():
-    pass
+    r = np.linspace(0, 3.0, 1000)
+    SIGMA: final = 1.0
+    EPSILON: final = 1.0
+    V = 4 * EPSILON * ((SIGMA/r)**12 - (SIGMA/r)**6) 
+
+    plt.plot(r, V)
+    
+    plt.ylim(-1.2, 2)
+    plt.xlim(0.9, 3)
+    plt.grid(True)
+    
+    plt.show()
 
 if __name__ == "__main__":
     main()
