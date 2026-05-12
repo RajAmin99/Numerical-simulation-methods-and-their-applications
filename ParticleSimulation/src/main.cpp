c008873c4a9a6cb9269651e4a68c655d75606890
RajAmin99 <rajamin2004@gmail.com>
2026-05-12 13:18:35 +0100

    Starting statistical mechanics section

--- ParticleSimulation/src/main.cpp
+++ ParticleSimulation/src/main.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
+#include <vector>
+#include <cmath>
+#include <cstdlib>
 
 // To simulate position of particles
 struct Position {
     double x, y, z;
 };
 
+int NumParticles = 150;
+
 double calc_energy() {
 
     // Calculation of a Particle's Energy
 
     double energy = 0.0;
+    
 
     return energy;
 }
