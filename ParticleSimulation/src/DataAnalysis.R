3962cd3e43f411d1fe3455cfc3dcb2f08c26d9eb
RajAmin99 <rajamin2004@gmail.com>
2026-05-14 17:57:04 +0100

    finished R code, generated graphs

--- ParticleSimulation/src/DataAnalysis.R
+++ ParticleSimulation/src/DataAnalysis.R
@@ -1,10 +1,51 @@
-# Libraries
+# Libraries ====
 library(ggplot2)
 
-# Setting Up Files
-
+# Setting Up Files ====
 particle.Energy.file <- "ParticleSimulation/data/energy_data.csv"
 particle.Energy <- read.csv(particle.Energy.file, header = T)
 attach(particle.Energy)
 
-plot()
+rdf.Data.file <- "ParticleSimulation/data/rdf_histogram.csv"
+rdf.Data <- read.csv(rdf.Data.file, header = T)
+attach(rdf.Data)
+
+# Total Energy vs. Cycle ====
+particle.Energy.plot <- ggplot(
+    particle.Energy,
+    aes(x = Cycle, y = TotalEnergy)) +
+    geom_line(color = "steelblue") +
+    geom_smooth(method = "loess",
+    color = "red",
+    se = F) +
+    labs(title = "System Equilibration: Total Energy vs. Cycles",
+         x = "Monte Carlo Cycle", 
+         y = "Total Energy") +
+    theme_minimal()
+
+## Saving
+ggsave("ParticleSimulation/data/particle_Energy_plot.png", plot = particle.Energy.plot, width = 8, height = 5)
+
+
+# Radial Distribution Function ====
+
+## Simulation parameters from main code
+density <- 0.8
+bin_width <- rdf.Data$Distance[2] - rdf.Data$Distance[1]
+
+# Normalize raw counts to g(r)
+rdf.Data$IdealCount <- (2 * pi * rdf.Data$Distance * bin_width) * density
+rdf.Data$g_r <- rdf.Data$Count / rdf.Data$IdealCount
+rdf.Data$g_r[is.nan(rdf.Data$g_r) | is.infinite(rdf.Data$g_r)] <- 0
+
+# Plot Radial Distribution Function
+rdf.Data.plot <- ggplot(rdf.Data, aes(x = Distance, y = g_r)) +
+    geom_line(color = "forestgreen", size = 1) +
+    geom_hline(yintercept = 1, linetype = "dashed", color = "black") + # Ideal gas limit
+    labs(title = "Radial Distribution Function g(r)",
+         x = "Distance (r)", 
+         y = "g(r)") +
+    theme_minimal()
+
+# Save the plot
+ggsave("ParticleSimulation/data/rdf_Data_plot.png", plot = rdf.Data.plot, width = 8, height = 5)
